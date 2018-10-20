3734b35846c8c7bb103dad1b7938a0ba84e70032
mattbrailsford <me@mattbrailsford.com>
2018-10-20 16:22:39 +0100

    Remove excerpt from blog post page

--- components/page_types/BlogPostPage.vue
+++ components/page_types/BlogPostPage.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <page-title :title="page.name" />
-    <div class="relative px-6 pb-6 sm:px-8 sm:pb-8 md:px-12 md:pb-12 xl:pb-24">
+    <div class="relative px-6 sm:px-8 md:px-12">
       <blog-post-meta-data :blog-post="page" class="pt-6" />
-      <blog-post-excerpt :blog-post="page" class="pt-6" />
     </div>
-    <content-blocks :blocks="page.blocks" class="sm:-mt-3 xl:-mt-12" />
+    <content-blocks :blocks="page.blocks" class="pt-6" />
   </div>
 </template>
 
