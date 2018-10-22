4c1c5260d3fa904af5f8273b6293c5d00c28fd06
mattbrailsford <me@mattbrailsford.com>
2018-10-22 08:21:03 +0100

    Tweaked button styles

--- components/ButtonLink.vue
+++ components/ButtonLink.vue
@@ -1,8 +1,7 @@
 <template>
-    <nuxt-link :to="to" class="inline-block border-green border-4 md:border-8 py-4 md:py-6 px-4 md:px-12 
+    <nuxt-link :to="to" class="inline-block border-green border-4 md:border-8 p-4 md:py-6 md:px-12 
         font-title font-bold text-xl md:text-3xl text-green no-underline
-        hover:bg-green hover:text-white trans trans-fast
-        w-full md:w-auto">
+        hover:bg-green hover:text-white trans trans-fast">
         <slot></slot>
     </nuxt-link>
 </template>
